9c365bc59623293add9760f1683ba508d21d972e
Jarblack <jharblack09@gmail.com>
2017-08-28 19:56:41 -0500

    Descripción

---
+++ Source/Backend/laravel/resources/vistas/pot.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>POT</title>
+	</head>
+	<body>
+	<style type="text/css">
+
+    header{
+
+color:blue;
+
+    }
+
+
+	</style>
+
+	<header>
+<?php
+echo "Plan de ordenamiento territorial";
+?>
+</header>
+
+	</body>
+	</html>
