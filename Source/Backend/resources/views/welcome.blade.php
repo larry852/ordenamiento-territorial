f46c6bb18310b82b5f74ee35a3e961bd10a6a563
giomuri <giovannymurillo91@gmail.com>
2017-09-04 21:04:21 -0500

    cambio en la vista

--- Source/Backend/resources/views/welcome.blade.php
+++ Source/Backend/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    esto es una prueba
                 </div>
 
                 <div class="links">
