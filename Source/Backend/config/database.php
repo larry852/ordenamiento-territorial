521c2e9974099770861055fed82e57419ff1a4df
larry852 <larryportocarrero@gmail.com>
2017-09-05 06:02:37 -0500

    DB default Postgresql

--- Source/Backend/config/database.php
+++ Source/Backend/config/database.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    // 'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
