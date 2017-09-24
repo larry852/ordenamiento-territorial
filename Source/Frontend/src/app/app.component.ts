6173e5f7a7faf3677e52873a5ba847bc993cbb98
giomuri <giovannymurillo91@gmail.com>
2017-09-24 16:53:52 -0500

    llenado de registros

--- Source/Frontend/src/app/app.component.ts
+++ Source/Frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({ 
+  selector: 'app-root',                 /*nombre del componente que se llama desde el index.html */
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {             /* Para el componente sea exportado */
+  title = 'app';                        /*Nombre que se puede llamar {{title}} desde otro lugar, por ej. Se llama interpolación*/
 }
--- Source/Frontend/src/app/app.module.ts
+++ Source/Frontend/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterializeModule } from 'angular2-materialize';
+import { MaterializeModule } from 'angular2-materialize'; /*Se declaro para usar Materialize */
 
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
+@NgModule({  
+  declarations: [                /*aquí se declaran los componentes*/
     AppComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule            /*Se declaro para usar Materialize */
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]     /*aquí el componente Main Component */
 })
 export class AppModule { }
