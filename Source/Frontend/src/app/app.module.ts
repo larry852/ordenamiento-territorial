75d26da1b6e207583870f631d36b6909f3eedfcf
neapi666 <neapi666@hotmail.com>
2017-09-24 20:01:39 -0500

    view departamentos componenrte

--- Source/Frontend/src/app/app.module.ts
+++ Source/Frontend/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize'; /*Se declaro para usar Materialize */
-
-
 import { AppComponent } from './app.component';
+import { DepartamentosComponent } from './departamentos.component';
 
 @NgModule({  
   declarations: [                /*aquí se declaran los componentes*/
-    AppComponent
+    AppComponent, DepartamentosComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule            /*Se declaro para usar Materialize */
+    MaterializeModule     /*Se declaro para usar Materialize */
   ],
   providers: [],
   bootstrap: [AppComponent]     /*aquí el componente Main Component */
---
+++ Source/Frontend/src/app/departamentos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departamentos',
+  templateUrl: './departamentos.component.html',
+  styles: [`
+  
+  * {
+    border: 1px solid red;
+    
+  }
+
+  
+  `]
+})
+export class DepartamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
