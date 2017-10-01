26556a26ed72d0558221d10d972897747168cae3
neapi666 <neapi666@hotmail.com>
2017-09-30 23:17:53 -0500

    pruebas enrutamiento

---
+++ Source/Frontend/src/app/inicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class InicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
