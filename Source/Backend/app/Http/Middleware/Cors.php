f7803d2b082abd68f577bc82c6deadc2457e7d53
larry852 <larryportocarrero@gmail.com>
2017-09-30 20:57:33 -0500

    Configuracion cors para endpoint departamentos

--- Source/Backend/app/Http/Middleware/Cors.php
+++ Source/Backend/app/Http/Middleware/Cors.php
@@ -9,7 +9,6 @@ class Cors {
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
