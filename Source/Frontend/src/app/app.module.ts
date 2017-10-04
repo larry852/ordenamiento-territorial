918e52af550963932679d4b38a933e88c181c2a9
neapi666 <neapi666@hotmail.com>
2017-10-03 23:15:06 -0500

    componentes

--- Source/Frontend/src/app/app.module.ts
+++ Source/Frontend/src/app/app.module.ts
@@ -5,22 +5,30 @@ import { HttpModule } from "@angular/http";
 import { AppComponent } from './app.component';
 import { DepartamentosComponent } from './departamentos.component';
 
-import { RouterModule, Routes} from '@angular/router';
-import { InicioComponent } from './inicio.component';  /*para enrutamiento de paginas*/
+import { RouterModule, Routes} from '@angular/router'; /*para enrutamiento de paginas*/
+import { InicioComponent } from './inicio.component';
+import { MunicipiosComponent } from './municipios.component';
+import { EquipoComponent } from './equipo.component';
+import { ZonasComponent } from './zonas.component';  
 
 @NgModule({  
   declarations: [                /*aquí se declaran los componentes*/
     AppComponent, 
     DepartamentosComponent, 
-    InicioComponent
+    InicioComponent, 
+    MunicipiosComponent, EquipoComponent, ZonasComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule,    /*Se declaro para usar Materialize */
     HttpModule,
     RouterModule.forRoot([
-      {path:'departamentos', component:DepartamentosComponent},
-      {path: 'inicio', component:InicioComponent}
+      {path: 'inicio', component:InicioComponent},
+      {path: 'equipo', component:EquipoComponent},
+      {path: 'departamentos', component:DepartamentosComponent},
+      {path: 'municipios', component:MunicipiosComponent},
+      {path: 'zonas', component:ZonasComponent}
+      
       /*Se agregan los componentes a enlazar*/
 
     ])
---
+++ Source/Frontend/src/app/municipios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-municipios',
+  templateUrl: './municipios.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class MunicipiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
