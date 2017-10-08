0d8a1fc8e0606d7a2a049beebaf48ddffc3bf1fb
larry852 <larryportocarrero@gmail.com>
2017-10-08 00:01:07 -0500

    Rename model user

--- Source/Backend/app/User.php
+++ Source/Backend/app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+class User extends Model
 {
     protected $fillable = [
         'username', 'first_name', 'last_name','email','password','avatar','avatar','gender','phone','institution','is_active','last_login','date_joined','if_staff',
