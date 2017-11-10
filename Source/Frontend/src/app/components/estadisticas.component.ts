34bbceddf9e64bf6607b65a90569e99fce0dd4c1
neapi666 <neapi666@hotmail.com>
2017-11-09 22:15:45 -0500

    ajuste gráfica

--- Source/Frontend/src/app/components/estadisticas.component.ts
+++ Source/Frontend/src/app/components/estadisticas.component.ts
@@ -8,8 +8,8 @@ import { Component} from '@angular/core';
 export class EstadisticasComponent  {
 
   // Pie
-  public pieChartLabels: string[] = ['Ginebra', 'Cali', 'Palmira'];
-  public pieChartData: number[] = [300, 500, 100];
+  public pieChartLabels: string[] = ['Amazonas', 'Antioquia', 'Arauca', 'Atlántico', 'Bolívar', 'Boyacá', 'Caldas', 'Caquetá', 'Casanare', 'Cauca', 'Cesar', 'Chocó', 'Córdoba', 'Cundinamarca', 'Guainía', 'Guaviare', 'Huila', 'La Guajira', 'Magdalena', 'Meta', 'Nariño', 'Norte de Santander', 'Putumayo', 'Quindío', 'Risaralda', 'San Andrés y Providencia', 'Santander', 'Sucre', 'Tolima', 'Valle del Cauca', 'Vaupés','Vichada'];
+  public pieChartData: number[] = [11, 125, 7, 23, 46, 123, 27, 16, 19, 42, 25, 31, 30, 116, 9, 4, 37, 15, 30, 29, 64, 40, 13, 12, 14, 2, 87, 26, 47, 42, 6, 4];
   public pieChartType: string = 'pie';
 
   // events
