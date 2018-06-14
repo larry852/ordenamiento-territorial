73ce618eb3c08aa1ae3acd533688b36da6822664
larry852 <larryportocarrero@gmail.com>
2018-06-13 21:48:24 -0500

    Resolve header access control allow

--- Source/Backend/app/Http/Middleware/Cors.php
+++ Source/Backend/app/Http/Middleware/Cors.php
@@ -8,7 +8,7 @@ class Cors {
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', '*');
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
