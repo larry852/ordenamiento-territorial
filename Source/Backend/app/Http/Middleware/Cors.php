0b7721a14980e2b7742345e4f4311fb54fa3952c
larry852 <larryportocarrero@gmail.com>
2018-06-13 21:55:36 -0500

    CORS

--- Source/Backend/app/Http/Middleware/Cors.php
+++ Source/Backend/app/Http/Middleware/Cors.php
@@ -4,11 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Cors {
+class Cors
+{
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization, X-Requested-With');
     }
 }
